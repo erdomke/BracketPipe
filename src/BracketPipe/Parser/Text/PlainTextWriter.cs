e7a00e34197545a0823c6a24ddb99eb906f1b8ad
Eric Domke <erdomke@gmail.com>
2017-03-01 13:35:15 -0500

    Fixing .Net 3.5 Build part 2

--- src/BracketPipe/Parser/Text/PlainTextWriter.cs
+++ src/BracketPipe/Parser/Text/PlainTextWriter.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 
 namespace BracketPipe
