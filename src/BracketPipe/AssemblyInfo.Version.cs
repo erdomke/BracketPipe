19533efa356a5c3111c94d2a2aa04dd8e78a92bf
Eric Domke <erdomke@gmail.com>
2018-07-17 15:26:45 -0400

    Fix build error

--- src/BracketPipe/AssemblyInfo.Version.cs
+++ src/BracketPipe/AssemblyInfo.Version.cs
@@ -5,6 +5,6 @@
 //------------------------------------------------------------------------------
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.7.6648.37186")]
-[assembly: AssemblyFileVersion("0.7.6648.37186")]
+[assembly: AssemblyVersion("0.7.6772.34984")]
+[assembly: AssemblyFileVersion("0.7.6772.34984")]
 
